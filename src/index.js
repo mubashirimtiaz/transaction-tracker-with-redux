cbeac50839e8e9a731074c9a7266d3e2fdf7f921
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2020-09-13 20:17:54 +0500

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,6 +1,16 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { transactionReducer } from "./transaction/transaction.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["transaction"],
+};
+
+const rootReducer = combineReducers({
   transaction: transactionReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
